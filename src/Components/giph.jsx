ddc16e8d2b5c0cd5ea68291e462db2629d8814f4
Mahammad <mehemmedabbasov123@gmail.com>
2024-05-22 10:13:39 +0400

    Push to workplace

--- src/Components/giph.jsx
+++ src/Components/giph.jsx
@@ -4,20 +4,26 @@ import { useEffect, useState } from "react"
 export default function Giph(){
     var [arrayOfGiphs,setArrayOfGiphs] = useState([])
 
-    useEffect(() => {
-        axios.get('https://api.giphy.com/v1/gifs/search?api_key=2kXqrIF6BvILPxLTiGrGH6bJ1VR0LXk7&q=cat&limit=6&offset=0&rating=g&lang=en&bundle=messaging_non_clips').then(response => 
+        axios.get('https://api.giphy.com/v1/gifs/search?api_key=2kXqrIF6BvILPxLTiGrGH6bJ1VR0LXk7&q=cat&limit=5&offset=0&rating=g&lang=en&bundle=messaging_non_clips').then(response => 
             {
                 for(let i in response.data.data){
                     console.log(response.data.data[i].embed_url)
                     setArrayOfGiphs(arrayOfGiphs => [...arrayOfGiphs, `https://media2.giphy.com/media/${response.data.data[i].id}/giphy.gif`])
                 }
             })
-    }, [])
+
     
     return(
         <>
             <div className="absolute w-[200px] h-[300px] flex rounded-[15px] flex-wrap items-center justify-evenly top-[100px] right-[0px] bg-[#EEF5FF]">
-                {arrayOfGiphs.map(item => <img className="w-[80px] cursor-pointer h-[80px]" key={item} src={item}></img>)}
+                <div className="w-[100%] h-[80%] flex flex-wrap items-center justify-evenly">
+                    {arrayOfGiphs.map(item => <img className="w-[80px] cursor-pointer h-[80px]" key={item} src={item}></img>)}
+                </div>
+
+                <div className="flex items-center justify-evenly w-[100%] h-[15%]">
+                    <input className="w-[60%] h-[100%] rounded-[15px] pl-[5px]" type="text" placeholder="Search gif..."></input>
+                    <button className="bg-[#B4D4FF] w-[35%] h-[50px] rounded-[15px]">Search</button>
+                </div>
             </div>
         </>
     )
